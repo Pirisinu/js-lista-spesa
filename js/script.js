45211aec82f012e32ac9503caa007063081b12ce
Pirisinu <palazzialessandro2@gmail.com>
2023-09-14 14:21:44 +0200

    [ADD]Svolgimento per visualizzare in console

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/* 
+- ***Svolgimento per visualizzare in console***
+1. Creo un array contenente gli elementi della spesa(listaSpesa)
+2. Dichiaro un contatore = 0
+3. Creo un ciclo while con la condizione (contatore < listaSpesa.length)
+4. Vado a fare un console.log di listaSpesa[contatore]
+5. Incremento il contatore (contatore++) dentro il ciclo per far in modo che la condizione dello WHILE si verifichi e non vado in contro a loop
+- ***Implementazione del codice in html***
+1a. Vado in html e mi creo una UL
+2a. Su js mi richiamo l'UL
+3a. Nel ciclo vado a creare i miei LI
+4a. andrò ad inserire il testo
+5a. Aggiungo l'LI all'UL
+*/
+
+//1.
+const listaSpesa = ['pane', 'pizza', 'burro', 'sugo', 'kiwi',];
+//2.
+let contatore = 0;
+//3.
+while(contatore < listaSpesa.length){
+  console.log(listaSpesa[contatore])
+  contatore++;
+}
